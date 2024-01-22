8626e2634b364c1e4b72c97bf8507ff630e51379
Alexander Bukin <alex.bukin@hotmail.com>
2024-01-22 14:08:42 +0000

    Client-side refactors

--- public/index.js
+++ public/index.js
@@ -9,21 +9,47 @@ socket.addEventListener('open', () => {
 
 const sendMessage = () => {
   const input = document.querySelector('.chat-input');
-  const payload = { username, type: 'message', text: input.value };
+  const payload = {
+    username,
+    type: 'message',
+    text: input.value,
+    sentAt: getTime(),
+  };
   socket.send(JSON.stringify(payload));
   input.value = '';
 };
 
+const getTime = () => {
+  const date = new Date();
+  const h = date.getHours() < 10 ? `0${date.getHours()}` : date.getHours();
+  const m =
+    date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes();
+  return `${h}:${m}`;
+};
+
 const renderMessage = (data) => {
-  const { text, username, currentUser } = JSON.parse(data);
+  const { text, username, currentUser, sentAt } = JSON.parse(data);
+
   const msgDiv = document.createElement('div');
+
+  const topRow = document.createElement('div');
+  topRow.classList.add('chat-message-top');
+
   const usernameSpan = document.createElement('span');
   usernameSpan.classList.add('chat-message-username');
   usernameSpan.innerText = username;
+  topRow.appendChild(usernameSpan);
+
+  const timeSpan = document.createElement('span');
+  timeSpan.classList.add('chat-message-time');
+  timeSpan.innerText = sentAt;
+  topRow.appendChild(timeSpan);
+
   const textSpan = document.createElement('span');
   textSpan.classList.add('chat-message-text');
   textSpan.innerHTML = text;
-  msgDiv.appendChild(usernameSpan);
+
+  msgDiv.appendChild(topRow);
   msgDiv.appendChild(textSpan);
   msgDiv.classList.add('chat-message');
   if (currentUser) msgDiv.classList.add('current-user');
